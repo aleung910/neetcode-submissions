93a36646ba08210eb4022d24b128e72c25a86f0f
Alice Leung <138654250+aleung910@users.noreply.github.com>
2026-04-22 15:54:03 -0400

    Add: minimum-window-with-characters - submission-2

---
+++ Data Structures & Algorithms/minimum-window-with-characters/submission-2.cpp
@@ -0,0 +1,45 @@
+class Solution {
+public:
+    string minWindow(string s, string t) {
+        
+        if(t.empty()) return "";
+        int startIdx = -1;
+        unordered_map<char,int> mp, mp2;
+        int minLen = INT_MAX;
+
+        //using unsigned char when dont know either -a or -A
+        for(char c:t){
+            mp[(unsigned char)c]++;
+        }
+
+        int have = 0, need = mp.size();
+        int l =0;
+        for(int r =l;r<s.size();r++){
+            char c = (unsigned char)s[r];
+            mp2[c]++;
+
+            if(mp.count(c) && mp[c] == mp2[c]){
+                have++;
+            }
+
+            //
+            while(have==need){
+                if(r-l+1 < minLen ){
+                    minLen = r-l+1;
+                    startIdx = l;
+                }
+
+                mp2[(unsigned char)s[l]]--;
+                //after removing left most
+                //check if we broke the requirment lose point
+                if(mp.count(s[l]) && mp2[s[l]] < mp[s[l]]){
+                    have--;
+                }
+                l++;
+            }
+
+        }
+        return startIdx == -1 ? "" : s.substr(startIdx, minLen);
+
+    }
+};
